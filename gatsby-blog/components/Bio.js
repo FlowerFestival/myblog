71e8ac5de6d7e1a1f2f07d5c9882a36b0cd9d0db
matianfu <matianfu@gmail.com>
2017-01-18 17:44:23 +0800

    update

---
+++ gatsby-blog/components/Bio.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { config } from 'config'
+import { rhythm } from 'utils/typography'
+import { prefixLink } from 'gatsby-helpers'
+import profilePic from './180-180.png'
+
+class Bio extends React.Component {
+  render () {
+    return (
+      <p
+        style={{
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={prefixLink(profilePic)}
+          alt={`author ${config.authorName}`}
+          style={{
+            float: 'left',
+            marginRight: rhythm(1/4),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+          }}
+        />
+        <strong>{config.authorName}</strong> 
+      </p>
+    )
+  }
+}
+
+export default Bio
